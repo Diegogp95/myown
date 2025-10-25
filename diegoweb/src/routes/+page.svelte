73b514830509236e21e62c23e12501c5144a2bfc
Diego Gallegos <diego.gallegos.14@sansano.usm.cl>
2025-10-25 01:39:17 -0300

    Initialize SvelteKit project with Tailwind CSS and basic structure

---
+++ diegoweb/src/routes/+page.svelte
@@ -0,0 +1,2 @@
+<h1>This will be my website</h1>
+<p>DIEGO GALLEGOSSSS</p>
---
+++ diegoweb/src/routes/Navbar.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+
+</script>
+<nav class="">
+	<ul class="flex flex-row justify-end">
+		<li class="px-4"><a href="/">Home</a></li>
+		<li class="px-4"><a href="/about">About</a></li>
+		<li class="px-4"><a href="/projects">Projects</a></li>
+		<li class="px-4"><a href="/blog">Blog</a></li>
+		<li class="px-4"><a href="/contact">Contact</a></li>
+	</ul>
+</nav>
