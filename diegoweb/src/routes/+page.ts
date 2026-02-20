3ec8d67da9e0b33844f980fbf840c260b3eae43c
Diego Gallegos <diego.gallegos.14@sansano.usm.cl>
2026-02-20 00:38:05 -0300

    feat: rollback to redirection in root page

--- diegoweb/src/routes/+page.ts
+++ diegoweb/src/routes/+page.ts
@@ -1,12 +1,7 @@
 import { redirect } from '@sveltejs/kit';
 
-/* Redirection only in development mode, as we delegate the language selection to the CDN in production. */
-/* Could this cause any issues? Maybe not, but it's something to keep in mind. */
-/* Keep in mind the root page doesn't have any content, it's just a redirection to the default language. */
+/* rollback to redirection, it does not cause conflict with CDN redirection */
 export function load() {
-	if (import.meta.env.DEV) {
-		throw redirect(307, '/en/');
-	}
+	throw redirect(307, '/en/');
 
-	return {};
 }
